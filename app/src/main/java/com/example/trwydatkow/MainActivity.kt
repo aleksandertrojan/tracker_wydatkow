b52d1d8d23e81ec851aae396d43094d4767f72d3
Aleksander Trojan <119168355+aleksandertrojan@users.noreply.github.com>
2023-04-01 21:23:53 +0200

    v0.4 dodane pola dodawania wydatków oraz 1/2 obsługi spinnera

--- app/src/main/java/com/example/trwydatkow/MainActivity.kt
+++ app/src/main/java/com/example/trwydatkow/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.example.trwydatkow
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val languages = resources.getStringArray(R.array.Languages)
 
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, languages)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    Toast.makeText(this@MainActivity,
+                        getString(R.string.selected_item) + " " +
+                                "" + languages[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+
+                }
+            }
+        }
     }
-
-
 }
