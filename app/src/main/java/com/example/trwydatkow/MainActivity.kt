4dfe45b18780da12ef41a0429b03cd67e71f8c0e
Aleksander Trojan <119168355+aleksandertrojan@users.noreply.github.com>
2023-04-07 13:21:45 +0200

    v0.5.2 (?stabilna) a drop-down calendar was added to the date field

--- app/src/main/java/com/example/trwydatkow/MainActivity.kt
+++ app/src/main/java/com/example/trwydatkow/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.trwydatkow
 
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.icu.math.BigDecimal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +19,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /////////////////////////////
+        //val myEditText = findViewById<EditText>(R.id.home_ukwota)
+        val decimalEditText = findViewById<TextView>(R.id.home_ureszta)
 
+        //val myDecimal = myEditText.text.toString().toBigDecimal()
+
+        val decimalValue = BigDecimal("10.9")
+        decimalEditText.setText(decimalValue.toString())
+
+        ////////////////////
+        val dateField = findViewById<EditText>(R.id.home_udata)
+
+        dateField.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, monthOfYear)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                dateField.setText(SimpleDateFormat("dd/MM/yyyy").format(calendar.time))
+            }
+            DatePickerDialog(this, dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        ///////////////////
 
         val kategoria = resources.getStringArray(R.array.kategoria)
         val spinner = findViewById<Spinner>(R.id.home_ukat)
